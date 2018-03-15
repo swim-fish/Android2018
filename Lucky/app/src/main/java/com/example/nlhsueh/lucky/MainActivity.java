0a8512d3f963c81d85e008e03736efade734b4a7
nlhsueh <nlhsueh@fcu.edu.tw>
2018-03-15 14:11:16 +0800

    no message

--- Lucky/app/src/main/java/com/example/nlhsueh/lucky/MainActivity.java
+++ Lucky/app/src/main/java/com/example/nlhsueh/lucky/MainActivity.java
@@ -4,17 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_hello = findViewById(R.id.tv_hello);
     }
 
     public void fn_showLuckyNumber (View view) {
-        Log.i("Hello");
+//        Log.i("show lucky", "lucky");
+//        Toast.makeText(MainActivity.this,"x", Toast.LENGTH_LONG).show();
+        Random r = new Random();
+        int i = r.nextInt(6)+1;
+
+        tv_hello.setText(Integer.toString(i));
+
+//        Toast.makeText(MainActivity.this, Integer.toString(i), Toast.LENGTH_SHORT).show();
 
     }
 }
