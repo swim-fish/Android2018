ffc6a81c166237df1049ecdb31ed6acbaff8c921
nlhsueh <nlhsueh@fcu.edu.tw>
2018-03-15 20:56:30 +0800

    Intent example: pass the parameter n to another activity and show different flower

---
+++ Lucky/app/src/main/java/com/example/nlhsueh/lucky/Main2Activity.java
@@ -0,0 +1,43 @@
+package com.example.nlhsueh.lucky;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ImageView img = findViewById(R.id.img_flower);
+
+        Intent intent = getIntent();
+        int luckyNum = intent.getIntExtra("n", 1);
+        Log.i("show lucky", Integer.toString(luckyNum));
+
+
+        switch (luckyNum) {
+            case 1:
+                img.setImageResource(R.drawable.f1);
+                break;
+            case 2:
+                img.setImageResource(R.drawable.f2);
+                break;
+            case 3:
+                img.setImageResource(R.drawable.f3);
+                break;
+            case 4:
+                img.setImageResource(R.drawable.f4);
+                break;
+            case 5:
+                img.setImageResource(R.drawable.f5);
+                break;
+            default:
+                img.setImageResource(R.drawable.f6);
+        }
+    }
+}
--- Lucky/app/src/main/java/com/example/nlhsueh/lucky/MainActivity.java
+++ Lucky/app/src/main/java/com/example/nlhsueh/lucky/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.nlhsueh.lucky;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,22 +14,27 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_hello;
+    Button btn_go;
+    int luckyNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv_hello = findViewById(R.id.tv_hello);
+        btn_go = findViewById(R.id.btn_go);
     }
 
     public void fn_showLuckyNumber (View view) {
-//        Log.i("show lucky", "lucky");
-//        Toast.makeText(MainActivity.this,"x", Toast.LENGTH_LONG).show();
         Random r = new Random();
-        int i = r.nextInt(6)+1;
+        luckyNumber = r.nextInt(6)+1;
+        Log.i("random ", Integer.toString(luckyNumber));
+        btn_go.setEnabled(true);
+        tv_hello.setText(Integer.toString(luckyNumber));
+    }
 
-        tv_hello.setText(Integer.toString(i));
-
-//        Toast.makeText(MainActivity.this, Integer.toString(i), Toast.LENGTH_SHORT).show();
-
+    public void fn_goNextAct (View view) {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra("n", luckyNumber);
+        startActivity(intent);
     }
 }
