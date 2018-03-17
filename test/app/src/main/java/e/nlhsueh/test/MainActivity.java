3dd475ac8a16276dfad6316d30b5ef669036a3c8
nlhsueh <nlhsueh@fcu.edu.tw>
2018-03-17 23:10:51 +0800

    add function

--- test/app/src/main/java/e/nlhsueh/test/MainActivity.java
+++ test/app/src/main/java/e/nlhsueh/test/MainActivity.java
@@ -1,7 +1,10 @@
 package e.nlhsueh.test;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fn_web(View v) {
+        Uri u = Uri.parse("http://www.google.com");
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(u);
+        startActivity(intent);
     }
 }
