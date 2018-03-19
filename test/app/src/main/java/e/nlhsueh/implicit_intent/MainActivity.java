c086a705208df52022934da8d7c75ef9c9570d7b
nlh 17 <nlhsueh@mail.fcu.edu.tw>
2018-03-19 10:40:06 +0800

    add fragment

---
+++ test/app/src/main/java/e/nlhsueh/implicit_intent/MainActivity.java
@@ -0,0 +1,24 @@
+package nlhsueh.implicit_intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void fn_web(View v) {
+        Uri u = Uri.parse("http://www.google.com");
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(u);
+        startActivity(intent);
+    }
+}
